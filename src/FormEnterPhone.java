71cb2f27169f12a12390d76dc479249246cb367a
AlexsanderM <malov.alexsander@yandex.ru>
2016-07-23 22:16:13 +0300

    add JFormattedTextField

--- src/FormEnterPhone.java
+++ src/FormEnterPhone.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 
 public class FormEnterPhone {
     private JPanel formEnterPhone;
-    private JPanel topPanek;
+    private JPanel topPanel;
     private JPanel centrPanel;
+    private JButton numberPhoneButton;
+    private JFormattedTextField numberPhoneField;
 
     FormEnterPhone () {
 
@@ -13,7 +17,31 @@ public class FormEnterPhone {
         return formEnterPhone;
     }
 
-    public JPanel getTopPanek() {
-        return topPanek;
+    public JPanel getTopPanel() {
+        return topPanel;
+    }
+
+    private void createUIComponents() {
+        try {
+            MaskFormatter mf = new MaskFormatter("+7 (###) ###-##-##");
+            numberPhoneField = new JFormattedTextField(mf);
+            numberPhoneField.setFocusLostBehavior(numberPhoneField.COMMIT);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNumber() {
+        try {
+            numberPhoneField.commitEdit();
+            return numberPhoneField.getValue().toString().replaceAll("\\D", "");
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public JButton getNumberPhoneButton() {
+        return numberPhoneButton;
     }
 }
